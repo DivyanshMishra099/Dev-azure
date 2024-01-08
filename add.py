43256f7f7736b020a7c6886ca9b42c136fdd10ae
DivyanshMishra099 <divyanshmishra5177@gmail.com>
2024-01-08 11:40:00 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,4 @@
+num1 =3
+num2 =4
+num3 =[D[D[D[D[D[D[D[D[sum[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dnum1 + num2
+print ("The SUM of {0} and {1} is {2}'[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D['[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[C .format(num1,num2,sum))
